43f04e6824faebeb1dabe92232290c7bbf6c1957
gwonjihun <aspire51@naver.com>
2022-08-11 09:38:40 +0900

    'temp'

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ print(cv2.__version__)
 
 img_path = 'cat.jpg'
 img = cv2.imread(img_path)
+img = cv2.resize(img,[640,480])
 
 print(type(img))
 print(img.shape)
 
 if img is not None:
-    cv2.imshow('title',img)
+    cv2.imshow(img_path,img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+else:
+    print("Error!")
